842823aceae9d71640015ed235d07a8adab8bf43
Lucast221 <teixeiralucas940@outlook.com>
2023-02-22 12:53:02 -0300

    Primeito commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Banco
+{
+    class Program
+    {
+        //Definição dos dados 
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount(personName: "Lucas");
+            bankAccount.Deposit(amount: 100);
+            bankAccount.Withdrawn(amount: 150);
+            bankAccount.ShowDetails();
+        }
+    }
+    // Definição de variáveis
+    public class BankAccount
+    {
+        private string personName;
+        private decimal balance;
+
+        public decimal Balance => balance;
+
+        //Criação de um construtor para dar entrada no 'personName', visto que ele é privado
+        public BankAccount(string personName)
+        {
+            this.personName = personName;
+        }
+        //Definição de métodos ou funções(ações que serão realizadas)
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+        //Impedindo a retirada de um valor maior que o caixa
+        public void Withdrawn(decimal amount)
+        {if (amount > balance)
+            {
+                Console.WriteLine(value: "Valor informado é maior que o saldo");
+                return;
+            }
+        balance -= amount; 
+        }
+        public void ShowDetails() 
+        {
+            Console.WriteLine(value: $"{personName} possui saldo de {balance:C2}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Projeto Banco
  Simulação simples de interação com banco em C#
+------------------------------------------------------
+## Descrição
+ Entrada e Saída de valores
+------------------------------------------------------
+## Tecnologias Utilizadas
+ Esse projeto foi criado utilizando as tecnologias:
+### Back-End
+ - [C#] (https://docs.microsoft.com/donwload)
+### Editor
+ - [Microsoft Visual Studio]
+------------------------------------------------------
+## Como rodar o projeto
+ Clone o projeto com o comando abaixo:
+```bash
+# Clone o repositório
+ >git clone 
+   # Entre no diretório
+ >cd [Projeto-Banco]
+   # Execute o projeto
+ >CTRL F5
+```
+------------------------------------------------------
+## Futuras Funcionalidades
+- [X] Realizar saque ou depósito
+- [ ] Consultar apenas o saldo
+- [ ] Emitir extrato
+
